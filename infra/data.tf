4bf1fc44ad163bc6505f4f68051903a869bce4af
Lucas Lima <lucaslima_miml@hotmail.com>
2025-02-12 12:25:05 -0300

    chore(data): change default to filter name

--- infra/data.tf
+++ infra/data.tf
@@ -1,5 +1,8 @@
 data "aws_security_group" "security_group_default" {
-  name = "default" 
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
 }
 
 data "aws_subnets" "subnets-tripradar" {
